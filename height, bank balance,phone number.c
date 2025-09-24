994ed2ecaed638d933e63d7bc8128f97f488af9c
Irenepaul800 <irenepaul800@gmail.com>
2025-09-24 12:42:13 +0300

    Add files via upload

---
+++ height, bank balance,phone number.c
@@ -0,0 +1,25 @@
+/*
+Name: Irene Mwethya paul 
+Reg No:CT101/G/26535/25
+Date:12 September 2025
+Description: height, balance, number 
+*/
+#include <stdio.h>;
+int main() {
+   int height ;
+    printf("enter my height  \n");
+    scanf ("%d ",& height );//prompt user inputs
+    printf("my height is %d centimeters", height); //display height 
+     
+  int  balance ;
+     printf("\nenter my balance \n");
+     scanf("%d",& balance );//prompt user input 
+     printf("my balance is  Ksh %d ", balance);//display balance 
+ 
+  int  number;
+     printf("\nenter my number \n");
+     scanf("%d",& number);// prompt user input   
+     printf("my number is  %d ");
+     
+     return 0;
+  }   
