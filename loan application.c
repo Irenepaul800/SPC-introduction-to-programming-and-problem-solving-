4c1c5b89306c331bf5935f984ffb1e02ff8eb5c5
Irenepaul800 <irenepaul800@gmail.com>
2025-09-24 12:38:08 +0300

    Add files via upload

---
+++ loan application.c
@@ -0,0 +1,28 @@
+/* 
+Name: Irene Mwethya paul 
+Reg:CT101/G/26535/25
+Date:23th September 2025
+Description: loan application illegibility 
+*/
+#include<stdio.h>
+int main ()
+{
+int age;
+    printf("enter age (in years): "); 
+    scanf("%d", &age);
+   printf("the age is %d",age);
+   
+ int annual_income ;
+   printf("\nenter_income:Ksh \n");
+   scanf("%d",& annual_income);
+   printf(" \nthe annual_income is %d", annual_income); 
+     
+ if( age >= 21& annual_income >=21000){
+ printf(" \ncongratulations, you qualify for a loan.");
+  } else{
+  printf("unfortunately,we are unable to offer you a loan");
+  }
+  
+   return 0;
+    
+    }
