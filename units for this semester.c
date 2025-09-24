ced9848674a91abdea32d8962f24edb976e4dbd4
Irenepaul800 <irenepaul800@gmail.com>
2025-09-24 12:41:06 +0300

    Add files via upload

---
+++ units for this semester.c
@@ -0,0 +1,19 @@
+//simple c program (semester units)single line comment
+/*
+Name: Irene Mwethya paul
+Reg No:CT101/G/26535
+Date:12 September 2025
+Description: units for this semester
+*/
+#include<stdio.h>;
+int main () { 
+    printf("unit 1: SPH 2101 Physics \n");
+    printf("unit 2: SPH 2107 Analogue Electronic \n");
+    printf("unit 3: UCU 2105 Fundamental of Ict \n");
+    printf("unit 4: UCU 2101 Communication skills  \n");
+        
+    printf("unit 5: SPM 2100 Discrete mathematics  \n");
+    printf("unit 6: SPM 2102 Introduction to computer Architecture \n");
+    printf("unit 7: SPM 2101 Introduction to programming and problem solving  \n");
+    printf("unit 8: SPM 2104 Mathematics for science \n");
+        }
